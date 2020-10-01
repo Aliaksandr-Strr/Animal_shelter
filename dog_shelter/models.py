730afe6d73880b36d87bc5273384118f377a1c99
unknown <1481169@mail.ru>
2020-10-01 10:14:49 +0300

    Добавлена валидация даты. Добавлено разделение прав

--- dog_shelter/models.py
+++ dog_shelter/models.py
@@ -1,8 +1,16 @@
+from datetime import date
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+def date_control(value):
+    today = date.today()
+    if value != today:
+        raise ValidationError('Дата добавления животного в базу может быть только текущей!')
 
 
 class Dog(models.Model):
     name = models.CharField(max_length=50)
     egs = models.FloatField()
     breed = models.CharField(max_length=50)
-    entry_date = models.DateField()
+    entry_date = models.DateField(validators=[date_control])
--- dog_shelter/views.py
+++ dog_shelter/views.py
@@ -1,52 +1,39 @@
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render
-from . import forms
-from . import models
+from .models import Dog
 
 
+# @permission_required('dog_shelter.add_dog')
 def animal(request):
-    dogs = models.Dog.objects.all()
+    dogs = Dog.objects.all()
     return render(request, 'dog_shelter/dog.html', {'dogs': dogs})
 
 
-# def add_animal(request):
-#     dog_form = forms.DogForm
-#     if request.method == 'POST':
-#         form = forms.DogForm(request.POST)
-#         if form.is_valid():
-#             print(2)
-#             dogs = models.Dog()
-#             dogs.name = request.POST.get('name')
-#             dogs.egs = request.POST.get('egs')
-#             dogs.breed = request.POST.get('breed')
-#             dogs.entry_date = request.POST.get('entry_date')
-#             dogs.save()
-#             return HttpResponseRedirect("/")
-#     return render(request, 'dog_shelter/add_dog.html', {"dog_form": dog_form, })
-
-
-class AnimalCreate(SuccessMessageMixin, CreateView):
-    model = models.Dog
+class AnimalCreate(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
+    permission_required = 'dog_shelter.add_dog'
+    model = Dog
     fields = ['name', 'egs', 'breed', 'entry_date']
     template_name = 'dog_shelter/create.html'
     success_url = reverse_lazy('dog:home')
     success_message = 'Животное успешно добавлено'
 
 
-class AnimalUpdateView(SuccessMessageMixin, UpdateView):
-    model = models.Dog
+class AnimalUpdateView(PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
+    permission_required = 'dog_shelter.add_dog'
+    model = Dog
     fields = ['name', 'egs', 'breed', 'entry_date']
     template_name = 'dog_shelter/update.html'
     success_url = reverse_lazy('dog:home')
     success_message = 'Данные успешно изменены'
 
 
-class AnimalDeleteView(SuccessMessageMixin, DeleteView):
-    model = models.Dog
+class AnimalDeleteView(PermissionRequiredMixin, SuccessMessageMixin, DeleteView):
+    permission_required = 'dog_shelter.add_dog'
+    model = Dog
     template_name = 'dog_shelter/delete.html'
     success_url = reverse_lazy('dog:home')
     success_message = 'Данные успешно удалены'
